361ded919736df65209f54ee5de6389a7bcdcb95
JohnKim04 <154724240+JohnKim04@users.noreply.github.com>
2024-01-12 23:19:33 -0800

    Add files via upload

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
-// Add JavaScript code for content script (to be injected into web pages) here
-console.log('Content script loaded.');
+function getAllText(node) {
+    var allText = "";
+    node.childNodes.forEach(function(childNode) {
+        if (childNode.nodeType === Node.TEXT_NODE) {
+            allText += childNode.textContent.trim() + " ";
+        } else {
+            allText += getAllText(childNode);
+        }
+    });
+    return allText;
+}
 
-// You can add logic here to scrape information from the product webpage
-// For example, extracting product name, ingredients, and company practices
-// Modify this script according to the structure of the web pages you want to analyze
+let bodyText = getAllText(document.body);
+console.log(bodyText);
